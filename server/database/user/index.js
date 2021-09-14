9012dc5341272b34c3b27f497170e3373c0e1803
Shashank Zade <shashankzade07@gmail.com>
2021-09-14 21:54:36 +0530

    Api creation

---
+++ server/database/user/index.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        fullname: { type: String, require: true },
+        email: { type: String, require: true },
+        password: { type: String },
+        address: [{ details: { type: String }, for: { type: String } }],
+        phoneNumber: [{ type: Number }],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
+    // check whether email exist
+    const checkUserByEmail = await UserModel.findOne({ email });
+    const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+  
+    if (checkUserByEmail || checkUserByPhone) {
+      throw new Error("User Already Exist...!");
+    }
+  
+    return false;
+  };
+
+
+export const UserModel = mongoose.model("Users", UserSchema);
