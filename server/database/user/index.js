aba272855256c2541e772ea642b84b97b611506f
Shashank Zade <shashankzade07@gmail.com>
2021-09-15 22:51:23 +0530

    Google Auth and Google Config

--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema(
     {
-        fullname: { type: String, require: true },
-        email: { type: String, require: true },
+        fullName: { type: String, required: true },
+        email: { type: String, required: true },
         password: { type: String },
         address: [{ details: { type: String }, for: { type: String } }],
         phoneNumber: [{ type: Number }],
@@ -13,17 +15,56 @@ const UserSchema = new mongoose.Schema(
     }
 );
 
+//Statics and Methods
+
+UserSchema.methods.generateJwtToken = function () {
+    return jwt.sign({ user: this._id.toString() }, "ZomatoAPP");
+};
+
+UserSchema.statics.findByEmailAndPassword = async ({ password, email }) => {
+    // check whether email exists
+    const user = await UserModel.findOne({ email });
+    if (!user) throw new Error("User does not exist!!!");
+
+    // Compare password
+    const doesPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!doesPasswordMatch) throw new Error("Invalid Password!!!");
+
+    return user;
+};
+
 UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
-    // check whether email exist
+    //check whether email exists
     const checkUserByEmail = await UserModel.findOne({ email });
     const checkUserByPhone = await UserModel.findOne({ phoneNumber });
-  
-    if (checkUserByEmail || checkUserByPhone) {
-      throw new Error("User Already Exist...!");
-    }
-  
-    return false;
-  };
 
+    if (checkUserByEmail || checkUserByPhone) {
+        throw new Error("User Already Exists...!");
+    }
+
+    return false;
+};
+
+UserSchema.pre("save", function (next) {
+    const user = this;
+
+    //password is modified
+    if (!user.isModified("password")) return next();
+
+    //password bcrypt salt
+    bcrypt.genSalt(8, (error, salt) => {
+        if (error) return next(error);
+
+        //hash the password
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if (error) return next(error);
+
+            //assigning hashed password
+            user.password = hash;
+            return next();
+        });
+    });
+});
 
 export const UserModel = mongoose.model("Users", UserSchema);
--- server/index.js
+++ server/index.js
@@ -1,34 +1,40 @@
-// Importing Env Variables
 require("dotenv").config();
-
-// Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
 
-// microservice routes
-import Auth from "./API/Auth";
+//configs
+import googleAuthConfig from "./config/google.config";
 
-// Database connection
+//Routes
+import Auth from "./API/Auth/index";
+
+//Database connection
 import ConnectDB from "./database/connection";
 
 const zomato = express();
 
-// application middlewares
+//passport config
+googleAuthConfig(passport);
+
 zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
-zomato.use(helmet());
 zomato.use(cors());
+zomato.use(helmet());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
 
-// Application Routes
+zomato.get("/", (req, res) => {
+    res.json({ message: "Setup Success" });
+});
+
 zomato.use("/auth", Auth);
 
-zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
-
 zomato.listen(4000, () =>
-  ConnectDB()
-    .then(() => console.log("Server is running 🚀"))
-    .catch(() =>
-      console.log("Server is running, but database connection failed... ")
-    )
+    ConnectDB()
+        .then(() => console.log("Server is up and running"))
+        .catch(() =>
+            console.log("Server is running, but database connection failed ...")
+        )
 );
